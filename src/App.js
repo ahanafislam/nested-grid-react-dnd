fe7e3a5ede13aca384ecba529615159b486bd637
ahanafislam <ahanafislam@gmail.com>
2022-08-09 20:16:19 +0600

    Create basic drag and drop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Container from './components/Container';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <DndProvider backend={HTML5Backend}>
+        <Container/>
+      </DndProvider>
     </div>
   );
 }
---
+++ src/components/Container.js
@@ -0,0 +1,152 @@
+import React, { useCallback, useState } from 'react';
+import { COLUMN, COMPONENT, SIDEBAR_ITEM, SIDEBAR_ITEMS } from '../constants';
+import initialData from '../initial-data';
+import SideBarItem from './SideBarItem';
+import DropZone from './DropZone';
+import shortid from 'shortid';
+import {
+    handleMoveSidebarComponentIntoParent,
+    handleMoveToDifferentParent,
+    handleMoveWithinParent
+} from '../helpers';
+import Row from './Row';
+
+const Container = () => {
+    const initialLayout = initialData.layout;
+    const initialComponents = initialData.components;
+    const [layout, setLayout] = useState(initialLayout);
+    const [components, setComponents] = useState(initialComponents);
+
+    const handleDrop = useCallback(
+        (dropZone, item) => {
+          console.log('dropZone', dropZone)
+          console.log('item', item)
+    
+          const splitDropZonePath = dropZone.path.split("-");
+          const pathToDropZone = splitDropZonePath.slice(0, -1).join("-");
+    
+          const newItem = { id: item.id, type: item.type };
+          if (item.type === COLUMN) {
+            newItem.children = item.children;
+          }
+    
+          // sidebar into
+          if (item.type === SIDEBAR_ITEM) {
+            // 1. Move sidebar item into page
+            const newComponent = {
+              id: shortid.generate(),
+              ...item.component
+            };
+            const newItem = {
+              id: newComponent.id,
+              type: COMPONENT
+            };
+            setComponents({
+              ...components,
+              [newComponent.id]: newComponent
+            });
+            setLayout(
+              handleMoveSidebarComponentIntoParent(
+                layout,
+                splitDropZonePath,
+                newItem
+              )
+            );
+            return;
+          }
+    
+          // move down here since sidebar items dont have path
+          const splitItemPath = item.path.split("-");
+          const pathToItem = splitItemPath.slice(0, -1).join("-");
+    
+          // 2. Pure move (no create)
+          if (splitItemPath.length === splitDropZonePath.length) {
+            // 2.a. move within parent
+            if (pathToItem === pathToDropZone) {
+              setLayout(
+                handleMoveWithinParent(layout, splitDropZonePath, splitItemPath)
+              );
+              return;
+            }
+    
+            // 2.b. OR move different parent
+            // TODO FIX columns. item includes children
+            setLayout(
+              handleMoveToDifferentParent(
+                layout,
+                splitDropZonePath,
+                splitItemPath,
+                newItem
+              )
+            );
+            return;
+          }
+    
+          // 3. Move + Create
+          setLayout(
+            handleMoveToDifferentParent(
+              layout,
+              splitDropZonePath,
+              splitItemPath,
+              newItem
+            )
+          );
+        },
+        [layout, components]
+    );
+
+    const renderRow = (row, currentPath) => {
+    return (
+        <Row
+            key={row.id}
+            data={row}
+            handleDrop={handleDrop}
+            components={components}
+            path={currentPath}
+        />
+    );
+    };
+
+    return (
+        <div className='body'>
+            <div className="sideBar">
+                {Object.values(SIDEBAR_ITEMS).map((sideBarItem, index) => (
+                    <SideBarItem key={sideBarItem.id} data={sideBarItem} />
+                ))}
+            </div>
+            <div className="pageContainer">
+                <div className="page">
+                    {
+                        layout.map((row, index) => {
+                            const currentPath = `${index}`;
+
+                            return (
+                              <React.Fragment key={row.id}>
+                                <DropZone
+                                  data={{
+                                    path: currentPath,
+                                    childrenCount: layout.length
+                                  }}
+                                  onDrop={handleDrop}
+                                  path={currentPath}
+                                />
+                                {renderRow(row, currentPath)}
+                              </React.Fragment>
+                            );
+                        })
+                    }
+                    <DropZone
+                        data={{
+                        path: `${layout.length}`,
+                        childrenCount: layout.length
+                        }}
+                        onDrop={handleDrop}
+                        isLast
+                    />       
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Container;
